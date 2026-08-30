ecb01d748a7fef1fd08dd79c51f50788df8f884f
Mathias <horvatmathias@gmail.com>
2026-08-30 01:20:45 -0300

    usuarios-duplicados

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -5,19 +5,30 @@ export const register = async (req, res) => {
         // console.log("BODY RECIBIDO:", req.body);
         const { first_name, last_name, email, password, role } = req.body;
 
-        if (!first_name, !last_name, !email, !password) {
+        if (!first_name || !last_name || !email || !password) {
             return res.status(400).json({
                 status: 'error',
                 message: 'Todos los campos son obligatorios'
             })
         }
 
+        const normalizedEmail = email.toLowerCase().trim()
+
+        const userExists = await UserModel.findOne({ email: normalizedEmail })
+
+        if (userExists) {
+            return res.status(409).json({
+                status: 'error',
+                message: 'Ya existe un usuario registrado con el mismo mail'
+            })
+        }
+
         const newUser = await UserModel.create({
-            first_name,
-            last_name,
-            email,
-            password,
-            role
+            first_name: first_name,
+            last_name: last_name,
+            email: normalizedEmail,
+            password: password,
+            role: 'user'
         });
 
         res.status(201).json({
