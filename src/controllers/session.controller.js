8d120bfec5895467743e9bcaffc76472b626b12d
Mathias <horvatmathias@gmail.com>
2026-08-30 01:29:41 -0300

    sinPassword

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -2,42 +2,45 @@ import UserModel from '../models/user.model.js';
 
 export const register = async (req, res) => {
     try {
-        // console.log("BODY RECIBIDO:", req.body);
         const { first_name, last_name, email, password, role } = req.body;
 
         if (!first_name || !last_name || !email || !password) {
             return res.status(400).json({
                 status: 'error',
                 message: 'Todos los campos son obligatorios'
-            })
+            });
         }
 
-        const normalizedEmail = email.toLowerCase().trim()
+        const normalizedEmail = email.toLowerCase().trim();
 
-        const userExists = await UserModel.findOne({ email: normalizedEmail })
+        const userExists = await UserModel.findOne({ email: normalizedEmail });
 
         if (userExists) {
             return res.status(409).json({
                 status: 'error',
                 message: 'Ya existe un usuario registrado con el mismo mail'
-            })
+            });
         }
 
         const newUser = await UserModel.create({
-            first_name: first_name,
-            last_name: last_name,
+            first_name,
+            last_name,
             email: normalizedEmail,
-            password: password,
+            password,
             role: 'user'
         });
 
+        const userResponse = newUser.toObject();
+        delete userResponse.password;
+
         res.status(201).json({
-            message: "Usuario registrado correctamente",
-            data: newUser
+            message: 'Usuario registrado correctamente',
+            data: userResponse
         });
+
     } catch (error) {
         res.status(400).json({
-            status: "Error",
+            status: 'Error',
             message: error.toString()
         });
     }
